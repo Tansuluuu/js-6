297d4934277ff36043c425e671fbb23a27e4ed11
Tansuluuu <tynalievatan@gmail.com>
2023-07-17 13:41:12 +0600

    coding js

---
+++ script.js
@@ -0,0 +1,57 @@
+
+// function square(arr) {
+//     const result = arr.map(it => it * it)
+//     return result
+// }
+// console.log(square([1, 2, 3, 0, 4, 5, 6]))
+
+// const array = [1, 2, 3, 0, 4, 5, 6]
+// console.log(array.map((el) => el > 0))
+
+// const array = [1, 2, -3, 0, 4, -5, 6]
+// console.log(array < 0 ? true : false)
+
+// const array = [1, 2, -3, 0, 4, 5, -6]
+// console.log(array.filter((el) => el < 0))
+
+// const array = [1, 2, -3, 0, 4, 5, -6]
+// console.log(array.filter((el) => el % 2 === 0))
+
+// const array = ['hello', 'sit', 'ipsum', 'javascript', 'python', 'sun', 'even']
+// console.log(array.filter((el) => el.length > 5))
+
+// const array = [1, 2, -3, 0, -4, 5, -6]
+// console.log(array)
+//
+
+// let arr = [1, 2, -3, 0, -4, 5, -6]
+//     .reduce(
+//     (a, c) => {
+//         if (c < 0) a[0] = a[0] + c;
+//         return a;
+//     },
+//     [0]
+// );
+//
+// console.log(arr);
+
+
+
+// const num = [1,2,3,4]
+// const array = ['a','b','c','d']
+// console.log(num.concat(array))
+
+
+// const num = [2, 3, 4, 5]
+// const str = 'hello world'
+// console.log(num.concat(str))
+
+
+// const num = [2, 3, 4, 5]
+// const str = 'hello world'.split('')
+// console.log(num.concat(str))
+
+
+// const num = [2, 3, 4, 5]
+// const str = 'hello world'.split('')
+// console.log(num.concat(str).concat(num))
